0a1361062720d5ac35727bd995b20afe3d7e4301
Luca Bertaggia <luca.bertaggia82@gmail.com>
2025-03-24 00:38:32 +0100

    Curios no longer required

--- src/main/java/com/lucab/repair_amulet/items/BasicRepairAmulet.java
+++ src/main/java/com/lucab/repair_amulet/items/BasicRepairAmulet.java
@@ -2,9 +2,8 @@ package com.lucab.repair_amulet.items;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
-import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
-public class BasicRepairAmulet extends Item implements ICurioItem {
+public class BasicRepairAmulet extends Item {
     public BasicRepairAmulet() {
         super(new Item.Properties().rarity(Rarity.COMMON).stacksTo(1));
     }
